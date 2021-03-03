bcae6c2fa21a2e70cca65dd2fa2d3221497bfb96
yao <yaoke259@outlook.com>
2021-03-03 16:47:26 +1100

    first attempt

---
+++ venv/bin/Lesson1.py
@@ -0,0 +1,19 @@
+#libaries can be imported from other objects to used existing code
+#This follows the format object.method.data
+
+"""
+1. some words are reserve
+2. variables can't begin with numbers
+3. control statements end with :
+4. expressions are delineated with ()
+5. arrays are accessed with []
+6. strings are delimited with ""
+7. code is only executed if indented correctly
+"""
+
+#an example fo importing form another library
+import sys
+
+sys.stdout.write("Hello big penis")
+
+#python -m py_compile HelloWorld.py is used to compile a code into bytecode
